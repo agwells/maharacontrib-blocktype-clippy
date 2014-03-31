4d75fabdaa302e5a23c30e8ac7a285ff041abb04
Aaron Wells <aaronw@catalyst.net.nz>
2014-03-31 18:51:56 +1300

    Update lib.php

--- lib.php
+++ lib.php
@@ -4,9 +4,8 @@
  *
  * @package    mahara
  * @subpackage blocktype-clippy
- * @author     Aaron Wells <francois@catalyst.net.nz>
+ * @author     Aaron Wells <aaronw@catalyst.net.nz>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
- * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
 
